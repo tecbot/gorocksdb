53464c3b4bd39fa31ec05dcb2cf1e25b511c28d6
Thomas Adam <tecbot@users.noreply.github.com>
2017-03-29 22:20:08 +0200

    update SeekForPrev comment

--- iterator.go
+++ iterator.go
@@ -100,7 +100,8 @@ func (iter *Iterator) Seek(key []byte) {
 	C.rocksdb_iter_seek(iter.c, cKey, C.size_t(len(key)))
 }
 
-// Seek moves the iterator to the last key that less than or equal to the target key, in contrast with Seek().
+// SeekForPrev moves the iterator to the last key that less than or equal
+// to the target key, in contrast with Seek.
 func (iter *Iterator) SeekForPrev(key []byte) {
 	cKey := byteToChar(key)
 	C.rocksdb_iter_seek_for_prev(iter.c, cKey, C.size_t(len(key)))
