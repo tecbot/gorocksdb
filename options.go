55ce39d543e8c91e387b8585888fdf205b2746b1
bibby <james.bibby@datadoghq.com>
2019-10-16 15:36:33 -0400

    Attempting to destroy the opts.cst will segfault
    Reference: https://github.com/facebook/rocksdb/issues/1095
    
    The cst has alredy been consumed when associated with the prefix_extractor

--- options.go
+++ options.go
@@ -1202,9 +1202,8 @@ func (opts *Options) Destroy() {
 	if opts.ccmp != nil {
 		C.rocksdb_comparator_destroy(opts.ccmp)
 	}
-	if opts.cst != nil {
-		C.rocksdb_slicetransform_destroy(opts.cst)
-	}
+	// don't destroy the opts.cst here, it has already been
+	// associated with a PrefixExtractor and this will segfault
 	if opts.ccf != nil {
 		C.rocksdb_compactionfilter_destroy(opts.ccf)
 	}
