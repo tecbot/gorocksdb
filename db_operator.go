49be1cad9ef150ecd6f3a7ad6c4fe70aaf03036a
eson.hsm <eson.hsm@nonolive.com>
2021-06-18 01:46:15 +0800

    feat(gob): tODO: MultiGetObject test all

--- db_operator.go
+++ db_operator.go
@@ -1,5 +1,12 @@
 package rocks
 
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"reflect"
+)
+
 // Operator easy to operate rocksdb
 type Operator struct {
 	db   *DB
@@ -11,6 +18,35 @@ type Operator struct {
 	cfhs map[string]*ColumnFamilyHandle
 
 	isDestory bool
+}
+
+func OpenDbColumnFamiliesEx(opts *Options, name string) (*Operator, error) {
+
+	cfnames, err := ListColumnFamilies(opts, name)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfOpts []*Options
+	for range cfnames {
+		cfOpts = append(cfOpts, opts)
+	}
+
+	db, cfs, err := OpenDbColumnFamilies(opts, name, cfnames, cfOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	op := &Operator{
+		db:   db,
+		opts: opts,
+		cfhs: make(map[string]*ColumnFamilyHandle),
+	}
+	for i, name := range cfnames {
+		op.cfhs[name] = cfs[i]
+	}
+
+	return op, nil
 }
 
 // GetSelf get object  *DB *ColumnFamilyHandle
@@ -73,14 +109,97 @@ type OperatorColumnFamily struct {
 	cfh  *ColumnFamilyHandle
 }
 
+// Put
+func (opcf *OperatorColumnFamily) Put(key, value []byte) error {
+	return opcf.db.PutCF(opcf.wopt, opcf.cfh, key, value)
+}
+
+// Put
+func (opcf *OperatorColumnFamily) PutObject(key []byte, value interface{}) error {
+	var buf bytes.Buffer
+	err := gob.NewEncoder(&buf).Encode(value)
+	if err != nil {
+		return err
+	}
+	return opcf.db.PutCF(opcf.wopt, opcf.cfh, key, buf.Bytes())
+}
+
+// GetObject is safe
+func (opcf *OperatorColumnFamily) GetObject(key []byte, value interface{}) error {
+	s, err := opcf.db.GetCF(opcf.ropt, opcf.cfh, key)
+	if err != nil {
+		return err
+	}
+	defer s.Free()
+	if s.Exists() {
+		return gob.NewDecoder(bytes.NewReader(s.Data())).Decode(value)
+	}
+	return nil
+}
+
+// MultiGetObject is safe
+func (opcf *OperatorColumnFamily) MultiGetObject(value interface{}, key ...[]byte) error {
+	rtype := reflect.TypeOf(value)
+	if rtype.Kind() != reflect.Slice {
+		return fmt.Errorf("value must be the type of slice")
+	}
+	rtype = rtype.Elem()
+	if rtype.Kind() == reflect.Ptr {
+		rtype = rtype.Elem()
+
+		rvalue := reflect.ValueOf(value)
+		zero := reflect.Zero(rtype)
+
+		ss, err := opcf.db.MultiGetCF(opcf.ropt, opcf.cfh, key...)
+		if err != nil {
+			return nil
+		}
+		defer ss.Destroy()
+		for _, s := range ss {
+			if s.Exists() {
+				item := reflect.New(rtype)
+				err = gob.NewDecoder(bytes.NewReader(s.Data())).DecodeValue(item)
+				if err != nil {
+					return err
+				}
+				rvalue = reflect.Append(rvalue, item.Addr())
+			} else {
+				rvalue = reflect.Append(rvalue, zero)
+			}
+		}
+	} else {
+		rvalue := reflect.ValueOf(value)
+		zero := reflect.Zero(rtype)
+
+		ss, err := opcf.db.MultiGetCF(opcf.ropt, opcf.cfh, key...)
+		if err != nil {
+			return nil
+		}
+		defer ss.Destroy()
+		for _, s := range ss {
+			if s.Exists() {
+				item := reflect.New(rtype)
+				err = gob.NewDecoder(bytes.NewReader(s.Data())).DecodeValue(item)
+				if err != nil {
+					return err
+				}
+				rvalue = reflect.Append(rvalue, item)
+			} else {
+				rvalue = reflect.Append(rvalue, zero)
+			}
+		}
+	}
+	return nil
+}
+
 // Get Slice is need free
 func (opcf *OperatorColumnFamily) Get(key []byte) (*Slice, error) {
 	return opcf.db.GetCF(opcf.ropt, opcf.cfh, key)
 }
 
 // Get Slices is need Destory
-func (opcf *OperatorColumnFamily) MultiGetCF(key []byte) (Slices, error) {
-	return opcf.db.MultiGetCF(opcf.ropt, opcf.cfh, key)
+func (opcf *OperatorColumnFamily) MultiGet(key ...[]byte) (Slices, error) {
+	return opcf.db.MultiGetCF(opcf.ropt, opcf.cfh, key...)
 }
 
 func (opcf *OperatorColumnFamily) GetSafe(key []byte, do func(value []byte)) error {
@@ -93,31 +212,17 @@ func (opcf *OperatorColumnFamily) GetSafe(key []byte, do func(value []byte)) err
 	return nil
 }
 
-func OpenDbColumnFamiliesEx(opts *Options, name string) (*Operator, error) {
-
-	cfnames, err := ListColumnFamilies(opts, name)
+// Get Slices is need Destory
+func (opcf *OperatorColumnFamily) MultiGetSafe(do func(i int, value []byte) bool, key ...[]byte) error {
+	ss, err := opcf.db.MultiGetCF(opcf.ropt, opcf.cfh, key...)
 	if err != nil {
-		return nil, err
+		return err
 	}
-
-	var cfOpts []*Options
-	for range cfnames {
-		cfOpts = append(cfOpts, opts)
+	defer ss.Destroy()
+	for i, s := range ss {
+		if !do(i, s.Data()) {
+			break
+		}
 	}
-
-	db, cfs, err := OpenDbColumnFamilies(opts, name, cfnames, cfOpts)
-	if err != nil {
-		return nil, err
-	}
-
-	op := &Operator{
-		db:   db,
-		opts: opts,
-		cfhs: make(map[string]*ColumnFamilyHandle),
-	}
-	for i, name := range cfnames {
-		op.cfhs[name] = cfs[i]
-	}
-
-	return op, nil
+	return nil
 }
