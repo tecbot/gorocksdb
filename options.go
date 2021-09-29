ed396bad2149a2d72dd5c4df7e46c662e75e2143
Dylan Forciea <dylan.forciea@datadoghq.com>
2021-09-29 15:22:32 -0500

    [gorocksdb] add SetAtomicFlush function for options

--- options.go
+++ options.go
@@ -1196,6 +1196,32 @@ func (opts *Options) SetOptimizeFiltersForHits(value bool) {
 	C.rocksdb_options_set_optimize_filters_for_hits(opts.c, C.int(btoi(value)))
 }
 
+// SetAtomicFlush sets atomic_flush
+// RocksDB supports atomic flush of multiple column families if the DB option
+// atomic_flush is set to true. The execution result of flushing multiple
+// column families is written to the MANIFEST with 'all-or-nothing' guarantee
+// (logically). With atomic flush, either all or no memtables of the column
+// families of interest are persisted to SST files and added to the database.
+//
+// This can be desirable if data in multiple column families must be consistent
+// with each other. For example, imagine there is one metadata column family
+// meta_cf, and a data column family data_cf. Every time we write a new record
+// to data_cf, we also write its metadata to meta_cf. meta_cf and data_cf must
+// be flushed atomically. Database becomes inconsistent if one of them is
+// persisted but the other is not. Atomic flush provides a good guarantee.
+// Suppose at a certain time, kv1 exists in the memtables of meta_cf and kv2
+// exists in the memtables of data_cf. After atomically flushing these two
+// column families, both kv1 and kv2 are persistent if the flush succeeds.
+// Otherwise neither of them exist in the database.
+//
+// Since atomic flush also goes through the write_thread, it is guaranteed that
+// no flush can occur in the middle of write batch.
+//
+// Default: false
+func (opts *Options) SetAtomicFlush(value bool) {
+	C.rocksdb_options_set_atomic_flush(opts.c, C.uchar(btoi(value)))
+}
+
 // Destroy deallocates the Options object.
 func (opts *Options) Destroy() {
 	C.rocksdb_options_destroy(opts.c)
