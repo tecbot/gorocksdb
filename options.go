f6dbac5c4de44a331ba2b833e5c753c592ab06ee
chenjiong <chenjiong@oppo.com>
2023-03-23 15:45:33 +0800

    feat(option): add write buffer manager and rate limiter feature support

--- options.go
+++ options.go
@@ -1207,6 +1207,25 @@ func (opts *Options) SetMaxSubCompactions(value int) {
 	C.rocksdb_options_set_max_subcompactions(opts.c, C.uint32_t(value))
 }
 
+// SetWriteBufferManager set write_bufffer_manager forthe option
+// The memory usage of memtable will report to this object. The same object
+// can be passed into multiple DBs and it will track the sum of size of all
+// the DBs. If the total size of all live memtables of all the DBs exceeds
+// a limit, a flush will be triggered in the next DB to which the next write
+// is issued.
+//
+// If the object is only passed to one DB, the behavior is the same as
+// db_write_buffer_size. When write_buffer_manager is set, the value set will
+// override db_write_buffer_size.
+//
+// This feature is disabled by default. Specify a non-zero value
+// to enable it.
+//
+// Default: null
+func (opts *Options) SetWriteBufferManager(w *WriteBufferManager) {
+	C.rocksdb_options_set_write_buffer_manager(opts.c, w.c)
+}
+
 // Destroy deallocates the Options object.
 func (opts *Options) Destroy() {
 	C.rocksdb_options_destroy(opts.c)
